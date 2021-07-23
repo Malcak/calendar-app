ef06a37cacb52e76d0cdc44518e1826c3ec8d64b
Malcak <malcakx@gmail.com>
2021-07-23 10:58:50 -0500

    feat: create <LoginScreen /> structure

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import './LoginScreen.css';
 
 export const LoginScreen = () => {
   return (
-    <div>
-      <h1>Login Screen</h1>
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-5 login-form-signin me-1">
+          <h3>Sign In</h3>
+          <form>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" className="btn-submit">
+              Login
+            </button>
+          </form>
+        </div>
+
+        <div className="col-md-5 login-form-signup">
+          <h3>Sign Up</h3>
+          <form>
+            <div className="mb-3">
+              <input type="text" className="form-control" placeholder="Name" />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Confirm password"
+              />
+            </div>
+            <button type="submit" className="btn-submit">
+              Create Account
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
