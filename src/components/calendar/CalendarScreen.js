56fbbcb304ed42a07f81a7fa6245f4713394c463
Malcak <malcakx@gmail.com>
2021-07-23 16:07:01 -0500

    feat: add <CalendarEvent /> as custom event component

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -5,6 +5,7 @@ import parse from 'date-fns/parse';
 import startOfWeek from 'date-fns/startOfWeek';
 import getDay from 'date-fns/getDay';
 import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const locales = {
@@ -21,6 +22,17 @@ const localizer = dateFnsLocalizer({
 const events = [];
 
 export const CalendarScreen = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: 0,
+      opacity: 0.8,
+      display: 'block',
+      color: 'white',
+    };
+    return { style };
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -29,6 +41,8 @@ export const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        eventPropGetter={eventStyleGetter}
+        components={{ event: CalendarEvent }}
       />
     </div>
   );
