207f7b1dac06b7af897aeb831a0a3ec73407aace
Malcak <malcakx@gmail.com>
2021-07-23 20:20:47 -0500

    feat: provide store from <App />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import { AppRouter } from './routers/AppRouter';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 }
 
